6bd3edee971f8c15283aa3c459b0435c636c9305
dm100usa-oss <dm100usa@gmail.com>
2025-11-12 18:50:06 -0500

    Create route.ts

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url><loc>https://ricardo-demi-books.vercel.app/</loc><priority>1.0</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/author</loc><priority>0.9</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/method</loc><priority>0.8</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/method/es</loc><priority>0.8</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/method/application</loc><priority>0.7</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/method/reviews</loc><priority>0.7</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/contact</loc><priority>0.6</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/books/how-to-draw-111</loc><priority>0.8</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/books/lucky-rocky</loc><priority>0.8</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/books/es/como-dibujar-111</loc><priority>0.8</priority></url>
+    <url><loc>https://ricardo-demi-books.vercel.app/books/es/rocky-afortunado</loc><priority>0.8</priority></url>
+  </urlset>`;
+
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
