2dd4837b154c8ceb896909323ab13fb1b2ad7bf4
dm100usa-oss <dm100usa@gmail.com>
2025-11-12 23:16:24 -0500

    Create route.ts

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const baseUrl = 'https://ricardo-demi-books.vercel.app'
+
+  const staticPages = [
+    '',
+    'author',
+    'awards',
+    'books',
+    'contact',
+    'knowledge',
+    'method',
+    'method/application',
+    'method/reviews',
+    'premios',
+    'es',
+  ]
+
+  const urls = staticPages
+    .map(
+      (page) => `
+        <url>
+          <loc>${baseUrl}/${page}</loc>
+          <changefreq>weekly</changefreq>
+          <priority>${page === '' ? '1.0' : '0.7'}</priority>
+        </url>`
+    )
+    .join('')
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls}
+  </urlset>`
+
+  return new NextResponse(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+    },
+  })
+}
