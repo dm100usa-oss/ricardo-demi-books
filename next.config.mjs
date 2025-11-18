e8c2d3e20ba1cfea907b8eb8d610a2589828e1d2
dm100usa-oss <dm100usa@gmail.com>
2025-11-17 22:36:26 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  trailingSlash: false,
+
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ["lucide-react"],
+  },
+
   images: {
     domains: [],
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
   },
 };
 
