e751cbb2380b7872d7e3811f68dd4dff668b4d3b
dm100usa-oss <dm100usa@gmail.com>
2025-11-24 15:25:34 -0500

    Update route.ts

--- app/api/value_profile_taxonomy/route.ts
+++ app/api/value_profile_taxonomy/route.ts
@@ -1,26 +1,6 @@
 import { NextResponse } from "next/server";
-import path from "path";
-import { promises as fs } from "fs";
+import data from "@/public/api/value_profile_taxonomy.json";
 
 export async function GET() {
-  try {
-    const filePath = path.join(
-      process.cwd(),
-      "public",
-      "api",
-      "value_profile_taxonomy.json"
-    );
-
-    const jsonData = await fs.readFile(filePath, "utf8");
-
-    return new NextResponse(jsonData, {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    return new NextResponse(
-      JSON.stringify({ error: "Unable to load value_profile_taxonomy.json" }),
-      { status: 500 }
-    );
-  }
+  return NextResponse.json(data);
 }
