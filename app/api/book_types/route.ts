bcfb5733a28b12f3cec404a3faf32588e126aedf
dm100usa-oss <dm100usa@gmail.com>
2025-11-24 15:26:16 -0500

    Update route.ts

--- app/api/book_types/route.ts
+++ app/api/book_types/route.ts
@@ -1,26 +1,6 @@
 import { NextResponse } from "next/server";
-import path from "path";
-import { promises as fs } from "fs";
+import data from "@/public/api/book_types.json";
 
 export async function GET() {
-  try {
-    const filePath = path.join(
-      process.cwd(),
-      "public",
-      "api",
-      "book_types.json"
-    );
-
-    const jsonData = await fs.readFile(filePath, "utf8");
-
-    return new NextResponse(jsonData, {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    return new NextResponse(
-      JSON.stringify({ error: "Unable to load book_types.json" }),
-      { status: 500 }
-    );
-  }
+  return NextResponse.json(data);
 }
