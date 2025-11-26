d8156976482ccd5daf804bd7bbf2f8db47233a92
dm100usa-oss <dm100usa@gmail.com>
2025-11-26 15:37:16 -0500

    Create page.tsx

---
+++ app/practice/page.tsx
@@ -0,0 +1,8 @@
+export default function PracticePage() {
+  return (
+    <div style={{ padding: "40px", maxWidth: "800px", margin: "0 auto" }}>
+      <h1>Practice</h1>
+      <p>This page is reserved for future use.</p>
+    </div>
+  );
+}
