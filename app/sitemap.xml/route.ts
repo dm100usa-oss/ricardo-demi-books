65515cb7fedeb0b185a5d964a75fa3155ac6f850
dm100usa-oss <dm100usa@gmail.com>
2026-01-18 12:15:19 -0500

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,42 +1,42 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const baseUrl = 'https://www.ricardo-demi.com'
+  const baseUrl = "https://www.ricardo-demi.com";
 
   const staticPages = [
-    '',
-    'author',
-    'awards',
-    'books',
-    'contact',
-    'knowledge',
-    'method',
-    'method/application',
-    'method/reviews',
-    'premios',
-    'es',
-  ]
+    "",
+    "author",
+    "awards",
+    "books",
+    "contact",
+    "knowledge",
+    "method",
+    "method/application",
+    "method/reviews",
+    "premios",
+    "es",
+  ];
 
   const urls = staticPages
     .map(
       (page) => `
-        <url>
-          <loc>${baseUrl}/${page}</loc>
-          <changefreq>weekly</changefreq>
-          <priority>${page === '' ? '1.0' : '0.7'}</priority>
-        </url>`
+    <url>
+      <loc>${baseUrl}/${page}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>${page === "" ? "1.0" : "0.7"}</priority>
+    </url>`
     )
-    .join('')
+    .join("");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${urls}
-  </urlset>`
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
 
   return new NextResponse(xml, {
     headers: {
-      'Content-Type': 'application/xml',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      "Content-Type": "application/xml",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
     },
-  })
+  });
 }
